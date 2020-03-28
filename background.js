888e10ef9a5550dbc8791426dc6ef22a4ed3da86
jpascas <jpascas@gmail.com>
2020-03-28 11:55:15 -0500

    additional pending features added

--- background.js
+++ background.js
@@ -1,39 +1,67 @@
 
 // declare array
-const mostRecentTabs = [];
+const mostRecentTabs = {};
+
+chrome.tabs.query({}, function(tabs) {
+  tabs.forEach(tab => {
+    if (!mostRecentTabs.hasOwnProperty(tab.windowId)) {
+    
+      mostRecentTabs[tab.windowId] = [];
+      console.log("assigning array", mostRecentTabs)
+    }
+    console.log(tab.windowId)
+  });
+});
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+  // since only one tab should be active and in the current window at once
+  // the return variable should only have one entry
+  let activeTab = tabs[0];
+  let activeTabId = activeTab.id; // or do whatever you need
+  let windowId = activeTab.windowId;
+
+  mostRecentTabs[windowId].push(activeTabId);
+  console.log("added initial tab", activeTabId);
+  console.log(activeTab)
+});
 
 // track most recent tabs
 chrome.tabs.onActivated.addListener((tab) => {
   // create array/object to store a tab each time it's activate
   console.log(`hello tab: ${tab.tabId}`);
 
+  let currentMostRecentTabs = mostRecentTabs[tab.windowId];
 
   // remove tab from array if it already exist
-  if (mostRecentTabs.includes(tab.tabId) === true) {
-    const index = mostRecentTabs.indexOf(tab.tabId);
+  if (currentMostRecentTabs.includes(tab.tabId) === true) {
+    const index = currentMostRecentTabs.indexOf(tab.tabId);
     if (index > -1) {
-      mostRecentTabs.splice(index, 1);
+      currentMostRecentTabs.splice(index, 1);
     }
   }
 
   // push most recent active tab to the end
-  mostRecentTabs.push(tab.tabId);
+  currentMostRecentTabs.push(tab.tabId);
 
   // remove oldest tab from the begining
   // only if length is greater than 5
-  if (mostRecentTabs.length > 5) {
-    mostRecentTabs.shift();
+  if (currentMostRecentTabs.length > 5) {
+    currentMostRecentTabs.shift();
   }
 
-  console.log(mostRecentTabs);
+  // mostRecentTabs[tab.windowId] = currentMostRecentTabs;
+
+  console.log(tab.windowId, mostRecentTabs[tab.windowId]);
 });
 
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.query({}, (tabs) => {
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    let currentMostRecentTabs = mostRecentTabs[tabs[0].windowId];
     // filter out mostRecentTabs
-    const tabsToClose = tabs.filter((x) => !mostRecentTabs.includes(x.id));
+    const tabsToClose = tabs.filter((x) => !currentMostRecentTabs.includes(x.id));
 
     console.log(tabsToClose);
 
@@ -42,5 +70,5 @@ chrome.browserAction.onClicked.addListener((tab) => {
 });
 
 // For Saturday
-// add current tab by defualt
-// track the window
+// add current tab by defualt (done)
+// track the window (done)
